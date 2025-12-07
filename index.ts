430a3be71ec67a3ef486fcb698fbdfe66a1fb342
Eric Harris <eahmedia22@gmail.com>
2025-12-07 08:42:18 -0500

    Moving the codebase to its own separate repository from the visualizer.

---
+++ index.ts
@@ -0,0 +1,5 @@
+import BracketEvent from "./lib/classes/BracketEvent"
+import BracketEntrant from "./lib/classes/BracketEntrant"
+import BracketSet from "./lib/classes/BracketSet"
+
+export { BracketEvent, BracketEntrant, BracketSet }
